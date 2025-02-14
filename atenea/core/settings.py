b9409bec414b6e35d10309f36194e34ce7405443
jpdevopti <juan.restrepo183@udea.edu.co>
2025-02-14 11:32:19 -0500

    update settings database

--- atenea/core/settings.py
+++ atenea/core/settings.py
@@ -75,6 +75,9 @@ DATABASES = {
         'PASSWORD': '@tene@2025',
         'HOST': 'localhost',  # O la dirección de tu servidor MySQL
         'PORT': '3306',  # Puerto por defecto de MySQL
+        'OPTIONS': {
+            'unix_socket': '/opt/bitnami/mariadb/tmp/mysql.sock',
+        },
     }
 }
 
