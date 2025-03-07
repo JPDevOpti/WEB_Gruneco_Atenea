b42a7572c6e1e7d1fdaa01bddae91e8507c11b5f
jpdevopti <juan.restrepo183@udea.edu.co>
2025-03-06 21:28:20 -0500

    Correción de las funsiones

--- atenea/apps/home/urls.py
+++ atenea/apps/home/urls.py
@@ -13,18 +13,15 @@ urlpatterns = [
     path('index/', views.index, name='index'),  # Página de inicio de Atenea
     path('login/', login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-<<<<<<< HEAD
     path('profile/', profile_view, name='profile'),  # Perfil de usuario
 
     # Registro de pacientes
     path('registro-demografico/', views.registro_demografico, name='registro_demografico'),
     path('pacientes/', views.lista_pacientes, name='lista_pacientes'),
-=======
 
     #resgistro de pacientes 
     path('registro_demografico/', views.registro_demografico, name='registro_demografico'),
     path('pacientes/', views.lista_pacientes, name='tables.html'),
->>>>>>> Release_1.0
     path('paciente/<int:paciente_id>/', views.detalle_paciente, name='detalle_paciente'),
     path('paciente/<int:numero_documento>/eliminar/', views.eliminar_paciente, name='eliminar_paciente'),
     path('paciente/<int:numero_documento>/editar/', views.editar_paciente, name='editar_paciente'),
@@ -32,7 +29,6 @@ urlpatterns = [
     # Exámenes médicos
     path('descargar-examen/<int:visita_examen_id>/', descargar_examen, name='descargar_examen'),
     path('examen/<int:visita_id>/<int:examen_id>/<int:paciente_id>/', views.realizar_examen, name='realizar_examen'),
-<<<<<<< HEAD
     path('guardar-examen/general/', views.guardar_examen_general_revisionsistemas, name='guardar_examen_general'),
     path('guardar-examen/fisico/', views.guardar_examen_fisico, name='guardar_examen_fisico'),
     path('guardar-examen/antecedentes/', views.guardar_examen_antecedentes, name='guardar_examen_antecedentes'),
@@ -41,7 +37,6 @@ urlpatterns = [
     path('proyectos/', views.proyectos, name='proyectos'),
     path('proyecto/agregar-visita/', views.agregar_visita, name='agregar_visita'),
     path('proyecto/<int:id>/eliminar/', views.eliminar_proyecto, name='eliminar_proyecto'),
-=======
     
     #Historia clinica examenes 
     path('guardar-examen-general/', views.guardar_examen_general_revisionsistemas, name='guardar_examen'),
@@ -59,7 +54,6 @@ urlpatterns = [
     #visitas
     path("agregar-visita/", views.agregar_visita, name="agregar_visita"),
     path('visita/eliminar/<int:id>/', views.eliminar_visita, name='eliminar_visita'),
->>>>>>> Release_1.0
 
     # Estadísticas
     path('estadisticas/', views.estadisticas, name='estadisticas'),
